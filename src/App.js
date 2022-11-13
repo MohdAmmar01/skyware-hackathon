f8145b9948633ff3d9ce6ae2c3ad199d354cfd80
MohdAmmar01 <mohdammar95822@gmail.com>
2022-11-14 01:50:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Routes,  Route} from "react-router-dom";
+import Solutions from "./components/Solutions";
+import Home from "./components/Home";
+import About from "./components/About";
+import Error from "./components/Error";
+import Testimonials from "./components/Testimonials";
+import Partners from "./components/Partners";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home/>} />
+      <Route exact path="/about" element={<About />} />
+      <Route exact path="/testimonials" element={<Testimonials />} />
+      <Route exact path="/contact" element={<Contact />} />
+      <Route exact path="/partners" element={<Partners />} />
+      <Route exact path="/solutions" element={<Solutions />} />
+      <Route exact path="*" element={<Error />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
